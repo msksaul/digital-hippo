b0aafaec3d6ff7b6a9f5de5ed79e1bc270946eec
mscalel <msksaul@gmail.com>
2024-03-07 16:14:31 -0600

    AirBnB Style Images

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -6,6 +6,7 @@ import { Skeleton } from './ui/skeleton'
 import Link from 'next/link'
 import { cn, formatPrice } from '../lib/utils'
 import { PRODUCT_CATEGORIES } from '../config'
+import ImageSlider from './ImageSlider'
 
 interface ProductListingProps {
   product: Product | null
@@ -40,10 +41,14 @@ const ProductListing = ({product, index}: ProductListingProps) => {
 
   const label = PRODUCT_CATEGORIES.find(({value}) => value === product.category)?.label
 
+  const validUrls = product.images.map(({image}) => (typeof image === 'string' ? image : image.url)).filter(Boolean) as string[]
+
   if(isVisible && product) {
     return (
       <Link href={`/product/${product.id}`} className={cn(`invisible h-full w-full cursor-pointer group/main`, {'visible animate-in fade-in-5': isVisible})}>
         <div className='flex flex-col w-full'>
+          <ImageSlider urls={validUrls}/>
+
           <h3 className='mt-4 font-medium text-sm text-gray-700'>{product.name}</h3>
           <p className='mt-1 text-sm text-gray-500'>{label}</p>
           <p className='mt-1 font-medium text-sm text-gray-900'>{formatPrice(product.price)}</p>
