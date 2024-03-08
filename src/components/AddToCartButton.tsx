52e939220c61167614aff4f9b108202f35e3d4dc
mscalel <msksaul@gmail.com>
2024-03-08 17:06:23 -0600

    Product Detail Page

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+
+const AddToCartButton = () => {
+
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false)
+    }, 2000);
+  
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [isSuccess])
+  
+
+  return (
+    <Button size={'lg'} className='w-full' onClick={() => setIsSuccess(true)}>
+      {isSuccess ? 'Added!' : 'Add to Cart'}
+    </Button>
+  )
+}
+
+export default AddToCartButton
